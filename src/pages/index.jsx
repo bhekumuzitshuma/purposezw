22bed2b51a9c2693636eb4805aa9ed7ba03e9259
andrea435 <147542841+andrea435@users.noreply.github.com>
2024-11-06 12:41:46 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import Layout from "@/components/Layout";
 import Hero from "@/components/Hero";
@@ -8,7 +9,19 @@ import ServicesSection from "@/components/ServicesSection";
 export default function Home() {
   return (
     <Layout>
-      {/* Hero */}
+      {/* SEO Meta Tags */}
+      <Head>
+        {/* Primary Meta Tags */}
+        <title>Your Site Title - Home</title>
+        <meta name="description" content="Purpose Vunani Asset Management works closely with Vunani Fund Managers and Vunani Private Clients who offer a range of multi-asset, specialist equity, property and bond funds in RSA, to bring the full range of products to Zimbabwe." />
+        <meta name="keywords" content="asset, management, purpose, vunani, zimbabwe" />
+        <meta name="author" content="Purpose Vunani Asset Management" />
+        
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Hero Section */}
       <section>
         <Hero />
       </section>
@@ -27,7 +40,7 @@ export default function Home() {
       <section></section>
 
       {/* Contact Details Section */}
-      <section className="bg-teal-100 ">
+      <section className="bg-teal-100">
         <div className="max-w-5xl mx-auto">
           <Contact />
         </div>
